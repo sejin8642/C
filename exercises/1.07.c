4f5fece9c01250da5fda4b6d061ed7e69fb8b629
Sejin Nam <sejinnam@Sejins-MacBook-Air.local>
2025-01-18 13:36:18 +0900

    trying to get rid of .swp file

--- exercises/1.07.c
+++ exercises/1.07.c
@@ -2,7 +2,7 @@
 
 int main () {
     // if you do not put \n at the end of the printf, % will be added to the end 
-    // of the output. % is from the terminal or shell, not from the program.
+    // of the output. % is from the termial or shell, not from the program.
     printf("%d\n", EOF); 
 
     return 0;
